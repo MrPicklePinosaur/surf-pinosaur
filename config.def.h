c04662c338df2c92d4f03857af51490572e2b375
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-08-04 12:47:18 -0400

    changed homepage

--- config.def.h
+++ config.def.h
@@ -207,4 +207,4 @@ static Button buttons[] = {
 	{ OnMedia,      MODKEY,         1,      clickexternplayer, { 0 },       1 },
 };
 
-#define HOMEPAGE "https://danieliu.xyz/"
+#define HOMEPAGE "https://searx.danieliu.xyz/"

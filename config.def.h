46bf9a4515528f8cea268f7a820a818fb28b5a35
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-08-03 22:44:23 -0400

    lul

--- config.def.h
+++ config.def.h
@@ -207,4 +207,4 @@ static Button buttons[] = {
 	{ OnMedia,      MODKEY,         1,      clickexternplayer, { 0 },       1 },
 };
 
-#define HOMEPAGE "https://duckduckgo.com/"
+#define HOMEPAGE "https://danieliu.xyz/"

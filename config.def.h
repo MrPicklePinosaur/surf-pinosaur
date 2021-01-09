1f04725ac4e99f377d90b21e487a5d019fafda08
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-01-09 13:39:27 -0500

    added vid notification

--- config.def.h
+++ config.def.h
@@ -106,7 +106,7 @@ static WebKitFindOptions findopts = WEBKIT_FIND_OPTIONS_CASE_INSENSITIVE |
 /* VIDEOPLAY(URI) */
 #define VIDEOPLAY(u) {\
         .v = (const char *[]){ "/bin/sh", "-c", \
-             "mpv --really-quiet \"$0\"", u, NULL \
+             "notify-send 'Opening video in mpv...' & mpv --really-quiet \"$0\"", u, NULL \
         } \
 }
 
